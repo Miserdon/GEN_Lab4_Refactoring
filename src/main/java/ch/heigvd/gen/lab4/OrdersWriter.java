02848a12382d5a2608ea1ddac6ab159c6f221ad3
Florian <florian.mulhauser@arcanite.ch>
2020-05-25 22:22:16 +0200

    bad smell Duplicate Code: the class Orders is just a copy of Order with an arrayList, Refactor: replace orders by ArrayList<Order> and then safly deletes class Order when it isn't used anymore

--- src/main/java/ch/heigvd/gen/lab4/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen/lab4/OrdersWriter.java
@@ -1,17 +1,19 @@
 package ch.heigvd.gen.lab4;
 
-public class OrdersWriter {
-    private Orders orders;
+import java.util.ArrayList;
 
-    public OrdersWriter(Orders orders) {
-        this.orders = orders;
+public class OrdersWriter {
+    private ArrayList<Order> orders = new ArrayList<>();
+
+    public OrdersWriter(ArrayList<Order> orders) {
+         this.orders = orders;
     }
 
     public String getContents() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
+        for (int i = 0; i < orders.size(); i++) {
+            Order order = orders.get(i);
             sb.append("{");
             sb.append("\"id\": ");
             sb.append(order.getOrderId());
@@ -30,7 +32,7 @@ public class OrdersWriter {
             sb.append("}, ");
         }
 
-        if (orders.getOrdersCount() > 0) {
+        if (orders.size() > 0) {
             sb.delete(sb.length() - 2, sb.length());
         }
 
